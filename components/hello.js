b9f1c5f480dec955cbb39686ba66f1faaa78daa7
bas.noblesse <81532419+FlipLucky@users.noreply.github.com>
2022-03-25 18:08:54 +0100

    added branch

---
+++ components/hello.js
@@ -0,0 +1,6 @@
+const hello = (name) => {
+    return `Hello ${name}!`
+}
+
+
+export default hello;
---
+++ main.js
@@ -0,0 +1,9 @@
+import hello from "./components/hello.js";
+
+const app = document.createElement('div');
+
+
+const helloApp = hello('Bas');
+
+console.log(helloApp) 
+
